164e25ba946fb09810520d3315baeec99f4362e3
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2023-04-05 22:26:30 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//         },
-//       },
-//       {
-//         test: /\.html$/,
-//         use: [
-//           {
-//             loader: 'html-loader',
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [
-//           MiniCssExtractPlugin.loader, 'css-loader',
-//         ],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './src/index.html',
-//       filename: './index.html',
-//     }),
-//     new MiniCssExtractPlugin({
-//       filename: 'style.css',
-//     }),
-//     new CleanWebpackPlugin(),
-//   ],
-// };
-
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -106,7 +59,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
-      favicon: './src/img/favicon.ico',
+      // favicon: './src/img/favicon.ico',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
